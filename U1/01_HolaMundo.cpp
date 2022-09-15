6b7352c23ff951c89b439ced1357639d865ca475
up210439 <up210439@alumnos.upa.edu.mx>
2022-09-15 09:51:08 -0500

    01_HolaMundo

---
+++ U1/01_HolaMundo.cpp
@@ -0,0 +1,25 @@
+/*
+Date 12/09/22
+Author: Adriana Marlene Silva López
+Email: up210692@alumnos.upa.edu.mx
+Description:
+MI primer programa en C++
+*/
+//--------------PREPROCESSOR DIRECTIVES----------
+
+//io-input/output y stream-flujo
+//Input/output library between the screen n´keyboard
+
+#include <iostream>
+
+//------MAIN FUNCTION----------
+
+int main (){
+     std::cout << "Hello world this is my first program" << std::endl;
+ 
+    //Prints on screen a text
+  
+    //Return 0 means that the program end succesfully
+    return 0;
+
+}
