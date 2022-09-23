b58986abf0e3ccaadf73539af45fc0ff07d13cfb
up210439 <up210439@alumnos.upa.edu.mx>
2022-09-23 10:01:41 -0500

    01_Renta

---
+++ U2/01_Renta.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int renta;
+    int impuesto;
+    cout << "Ingresa tu renta anual: \n";
+    cin >> renta;
+
+    if (renta < 10000){
+        impuesto = renta*.05;
+        cout << "Tu impositivo es: \n" << impuesto << endl;
+    }
+    if ((renta >=10000) && (renta <= 20000)){
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
