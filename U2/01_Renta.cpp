2383c128d68313db3758c9e8ed34d25cc386f0b3
up210439 <up210439@alumnos.upa.edu.mx>
2022-09-26 09:50:08 -0500

    01_Renta

--- U2/01_Renta.cpp
+++ U2/01_Renta.cpp
@@ -7,22 +7,27 @@ int main() {
     cin >> renta;
    
     if(renta < 10000){
+        cout << "Tu porcentaje de impositivo es: \n5%";
         impuesto = renta*.05;
     }
     if((renta >= 10000) && (renta < 20000)){
+        cout << "Tu porcentaje de impositivo es: \n15%";
         impuesto = renta*.15;
     }
     if((renta >= 20000) && (renta < 35000)){
+        cout << "Tu porcentaje de impositivo es: \n20%";
         impuesto = renta*.20;
     }
     if((renta >= 35000) && (renta < 60000)){
+        cout << "Tu porcentaje de impositivo es: \n30%";
         impuesto = renta*.30;
     }
     if( renta >= 60000){
+        cout << "Tu porcentaje de impositivo es: \n45%";
         impuesto = renta*.45;
     }
    
-    cout << "Tu impositivo es: \n" << impuesto << endl;
+    cout << "\nTu impositivo es: \n$" << impuesto << " pesos" << endl;
    
     return 0;
 }
