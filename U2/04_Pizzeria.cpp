9cdd38cdd0428946e55e4ee4607ed0a784ce4927
up210439 <up210439@alumnos.upa.edu.mx>
2022-09-29 10:37:28 -0500

    Pizzeria

--- U2/04_Pizzeria.cpp
+++ U2/04_Pizzeria.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 using namespace std;
 int main (){
-    char pizza;
-   
-
-
-    cout << "¿Tu pizza seria vegetariana o no vegetariana?"
-    cin << pizza;
+    char respuesta;
+    char extra;
     
+
+
+    cout << "¿Tu pizza seria vegetariana o no vegetariana v/n?\n";
+    cin >> respuesta;
+    
+    if((respuesta == 'v') || (respuesta == 'V')){
+        cout << "Ingredientes: \n1.-Pimiento\n2.-Tofu\n";
+    }
+    else if((respuesta == 'n') || (respuesta == 'N')){
+        cout << "Ingredientes: \n 1.- Peperoni \n 2.- Salmón \n 3.- Jamón\n";
+    }
+    else cout << "Pizza inexistente";
+
+    cout << "¿Que ingrediente extra lleva tu pizza?\n";
+    cin >> extra;
+    cout << "Tu pedido final es:\n 1.- Mozzarella\n2.-Tomate\n" << extra <<endl;
+    
+    
+
+    return 0;
 }
