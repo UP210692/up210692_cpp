6126018ed55a74f9e0cd8d5603088f84d6cb6b73
up210692 <up210692@alumnos.upa.edu.mxsiup210692kgit config --global user.email git config --global user.email up210692@alumnos.upa.edu.mx>
2022-10-05 10:37:43 -0500

    Temperatura

--- U2/05_Temp.cpp
+++ U2/05_Temp.cpp
@@ -4,13 +4,22 @@ using namespace std;
 int main(){
     int contador;
     float temperatura;
-    float AcumTemp=0;    
+    float AcumTemp=0;
+    float promedio;
+    float suma;    
     do{
-        cout << "Give me the temperature:";
+        cout << "Give me the temperature:\n";
         cin >> temperatura;
         AcumTemp+=temperatura;
         contador ++;
     }while(contador < 6);
+    suma = temperatura;
+    cout <<"suma"<< temperatura;
+    cout <<"El promedio de las temperaturas es:"<<endl;
+     
+
+    
+
 
     return 0;
 }
