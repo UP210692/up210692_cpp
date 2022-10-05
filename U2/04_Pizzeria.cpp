fb881e22772d643d2396933ac1f30a2a14d672f0
up210692 <up210692@alumnos.upa.edu.mxsiup210692kgit config --global user.email git config --global user.email up210692@alumnos.upa.edu.mx>
2022-10-04 23:49:48 -0500

    Pizzeria

--- U2/04_Pizzeria.cpp
+++ U2/04_Pizzeria.cpp
@@ -1,26 +1,38 @@
 #include <iostream>
 using namespace std;
 int main (){
-    char respuesta;
-    char extra;
+    double ingrediente;
+    char tipo;
     
-
-
-    cout << "¿Tu pizza seria vegetariana o no vegetariana v/n?\n";
-    cin >> respuesta;
+cout<<"Bienvenido a la pizzeria Bella Napoli.\nTipos de pizza\n\t1- Vegetariana\n\t2- No vegetariana\n";
+cout << "Introduce el número correspondiente al tipo de pizza que quieres:";
+cin >> tipo;
+if (tipo == '1'){
+    cout <<"Ingredientes de pizzas vegetarianas\n\t 1- Pimiento\n\t2- Tofu\n";
+    cout <<"Introduce el ingrediente que deseas: ";
+    cin >> ingrediente;
+}
+    if (ingrediente == '1'){
+        cout <<"Pizza vegetariana con mozzarella, tomate y pimiento"<<endl;
+    }
+    else if (ingrediente = '2'){
+        cout <<"Pizza vegetariana con mozzarella, tomate y tofu"<<endl;
+    }
+    else cout << "Ingrediente no disponible";
+if (tipo == '2') {
+    cout<<"Ingredientes de pizzas no vegetarianas\n\t1- Peperoni\n\t2- Jamón\n\t3- Salmón\n";
+    cout <<"Introduce el ingrediente que deseas: ";
+    cin >> ingrediente;
+    cout <<"Pizza no vegetarina con mozarrella, tomate y " <<ingrediente<<endl;
+}
+    if (ingrediente == '1'){
+        ingrediente = 'peperoni';
+    }
+    else if (ingrediente == '2'){
+        cout<<"jamón";
+    }
+    else cout <<"salmón";
     
-    if((respuesta == 'v') || (respuesta == 'V')){
-        cout << "Ingredientes: \n1.-Pimiento\n2.-Tofu\n";
-    }
-    else if((respuesta == 'n') || (respuesta == 'N')){
-        cout << "Ingredientes: \n 1.- Peperoni \n 2.- Salmón \n 3.- Jamón\n";
-    }
-    else cout << "Pizza inexistente";
-
-    cout << "¿Que ingrediente extra lleva tu pizza?\n";
-    cin >> extra;
-    if (extra == 1)
-    cout << "Tu pedido final es:\n 1.- Mozzarella\n2.-Tomate\n" << extra <<endl;
     
     
 
