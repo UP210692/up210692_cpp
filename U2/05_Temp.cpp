c5715daf07f62edc327f4d4df6d50155f7dcae6f
up210692 <up210692@alumnos.upa.edu.mxsiup210692kgit config --global user.email git config --global user.email up210692@alumnos.upa.edu.mx>
2022-10-13 21:10:12 -0500

    subiendo

--- U2/05_Temp.cpp
+++ U2/05_Temp.cpp
@@ -4,22 +4,36 @@ using namespace std;
 int main(){
     int contador = 1;
     float temperatura;
+    float temp1;
     float AcumTemp=0;
     float promedio;
-    float suma;    
+    float suma;
+    int may = INT8_MIN;
+    int men = INT8_MAX; 
+    //float i;   
     do{
         cout <<"Give me the temperature:\n";
-        cin >> temperatura;
+        cin >> temperatura; 
+    temp1=temperatura;
+        if (temperatura<men)
+        {
+            men=temperatura;
+        }
+        if (temperatura>men)
+        {
+            may=temperatura;
+        } 
+        suma = suma + temp1;
+        promedio=suma/6;
         AcumTemp+=temperatura;
-        promedio == AcumTemp/6;
         contador ++;
     }while(contador <= 6);
-cout << "Promedio" << promedio<<endl; 
 
-     
-
-    
-
+    cout << "Promedio:\n"<<promedio<<endl; 
+    cout <<"Suma:\n"<<suma<<endl;
+ 
+   cout << "La temperatura mayor es: \n"<<may <<endl;
+    cout<<"La temperatura menor es: \n"<< men <<endl;
 
     return 0;
 }
