aaa1ba913f5ae2b00be65c282bb429a537c564eb
up210692 <up210692@alumnos.upa.edu.mxsiup210692kgit config --global user.email git config --global user.email up210692@alumnos.upa.edu.mx>
2022-10-13 21:46:37 -0500

    subiendo

---
+++ U2/06_Factura.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (){
+    char productos;
+    float precio;
+    int contadorprecio = 1;
+    float Acumproductos = 0;
+    float Acumprecios = 0;
+    int contadorproducto = 1;
+    do()
+    {
+        /* code */
+    } while (/* condition */);
+    
+    return 0;
+}
