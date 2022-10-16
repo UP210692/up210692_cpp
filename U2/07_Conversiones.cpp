899d9067a2106bbe17c6262cdb3591c609450629
up210692 <up210692@alumnos.upa.edu.mxsiup210692kgit config --global user.email git config --global user.email up210692@alumnos.upa.edu.mx>
2022-10-15 23:13:54 -0500

    subiendo

--- U2/07_Conversiones.cpp
+++ U2/07_Conversiones.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 int main (){
+    int num;
+    int resultado;
 
+    cout << "Enter the number\n";
+    cin >> num;
+    while (num != 0)
+    {
+        if (num%2 == 0)
+        {
+            resultado = '0' + resultado;
+        }
+        
+    }
+    cout << "Resultado: \n" << resultado << endl;
+    
     
 }
