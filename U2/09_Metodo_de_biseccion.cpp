3f760fd8f0cf0edc3df74054f2e17347b159bbdf
up210692 <up210692@alumnos.upa.edu.mxsiup210692kgit config --global user.email git config --global user.email up210692@alumnos.upa.edu.mx>
2022-10-17 10:35:21 -0500

    subiendo

--- U2/09_Metodo_de_biseccion.cpp
+++ U2/09_Metodo_de_biseccion.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 using namespace std;
 int main (){
+    float a;
+    float b;
+    float c;
+    float ya;
+    float yb;
+    float yc;
 
-do
-{
+    cout << "Enter the value of a\n";
+    cin >> a;
+    cout << "Enter the value of b\n";
+    cin >> b;
+    
+    c = a+b/2;
+    cout<< "hhh"<<c;
+    do
+    {   
 
-    if(ya*yc<0)
+        if(ya*yc<0)
         b=c;
-    else if(yc*yb<0)
+        else if(yc*yb<0)
         a=c;
-    else cout<<"No exist"
-}
-while(yc>-0.01 || yc<0.01);
+        else cout<<"No exist";
+    }
+    while(yc>-0.01 || yc<0.01);
 
 }
