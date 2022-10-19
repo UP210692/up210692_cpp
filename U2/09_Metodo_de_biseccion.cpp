eef2773a9141f8ce22c67707f1cf0ae4a4e8fafa
up210692 <up210692@alumnos.upa.edu.mxsiup210692kgit config --global user.email git config --global user.email up210692@alumnos.upa.edu.mx>
2022-10-18 20:54:37 -0500

    subiendo

--- U2/09_Metodo_de_biseccion.cpp
+++ U2/09_Metodo_de_biseccion.cpp
@@ -1,29 +1,25 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+float ecuacion(float numero)
+{
+    return (pow(numero, 2)-numero-6);
+}
+
 int main (){
-    float a;
-    float b;
-    float c;
-    float ya;
-    float yb;
-    float yc;
+    float a,b,c,ya,yb,yc;
 
     cout << "Enter the value of a\n";
     cin >> a;
     cout << "Enter the value of b\n";
     cin >> b;
     
-    c = a+b/2;
-    cout<< "hhh"<<c;
-    do
-    {   
-
-        if(ya*yc<0)
-        b=c;
-        else if(yc*yb<0)
-        a=c;
-        else cout<<"No exist";
-    }
-    while(yc>-0.01 || yc<0.01);
-
+    c = (a+b)/2;
+    ya = ecuacion(a);
+    yb = ecuacion(b);
+    yc = ecuacion(c); 
+    cout<< "c="<<c<<endl;
+    cout << "ya="<<yb<<endl; 
+    return 0;
 }
