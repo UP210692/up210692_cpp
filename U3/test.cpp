132bd1f0222b8266b985d54934d444f4ecfe506e
up210439 <up210439@alumnos.upa.edu.mx>
2022-10-26 10:40:15 -0500

    subiendo

---
+++ U3/test.cpp
@@ -0,0 +1,24 @@
+int main (){
+    int numeros [100] = (0);
+    char c[5];
+
+    for (int contador; contador<6; contador++)
+    {
+        cout <<"Dame un valor"<<contador
+        cin <<c[contador];
+    }
+    cout << endl;
+for (int contador; contador<6; contador++)
+    cout <<c[constador]<<"";
+
+    get char ();
+}
+
+//mejorar 
+void llenarvector (int v[], int tamaño){
+    int numeros [100] = (0);
+    char c[5];
+
+    llenarvector(numeros,6);
+    
+}
