bc95c4b68a66cd7e52f94625cfcd66dc1a90b601
up210692 <up210692@alumnos.upa.edu.mxsiup210692kgit config --global user.email git config --global user.email up210692@alumnos.upa.edu.mx>
2022-10-25 19:58:08 -0500

    subiendo

--- U3/Cambio.cpp
+++ U3/Cambio.cpp
@@ -17,28 +17,36 @@ int diez = 0, cinco = 0, dos = 0, uno = 0;
 int main(){
 int dinero;
 int residuo;
-cout << "Cantidad de dinero";
+cout << "Cantidad de dinero \n";
 cin >> dinero;
 
 quinientos = dinero/500;
 residuo = dinero%500;
+cout <<"Billetes de quinientos:\n" << quinientos << endl;
 dosientos = residuo/200;
 residuo = residuo%200;
+cout << "Billetes de doscientos:\n"<<dosientos<<endl;
 cien = residuo/100;
 residuo = residuo%100;
+cout << "Billetes de cien:\n"<<cien<<endl;
 cincuenta = residuo/50;
 residuo = residuo%50;
+cout << "Billetes de cincuenta:\n"<<cincuenta<<endl;
 veinte = residuo/20;
 residuo = residuo%20;
+cout << "Billetes de veinte:\n"<<veinte<<endl;
 diez = residuo/10;
 residuo = residuo%10;
+cout << "Monedas de diez:\n"<<diez<<endl;
 cinco = residuo/5;
 residuo = residuo%5;
+cout << "MOnedas de cinco:\n"<<cinco<<endl;
 dos = residuo/2;
 residuo = residuo%2;
+cout << "Monedas de dos:\n"<<dos<<endl;
 uno = residuo/1;
 residuo = residuo%1;
-
+cout << "Monedas de peso:\n"<<uno<<endl;
 /*obtenercambio (dinero)
 imrimpircambio ("total")
 
