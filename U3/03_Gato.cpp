661589d97f469cbbac14f390caa2d9f6dd86787d
up210439 <up210439@alumnos.upa.edu.mx>
2022-11-03 10:21:30 -0600

    subiendo

---
+++ U3/03_Gato.cpp
@@ -0,0 +1,14 @@
+/*
+Autor: Adriana Marlene Silva Lopez
+Fecha: 03/Noviembre/2022
+Description: Este programa realiza el juego del gato 
+*/
+
+#include <iostream>
+using namespace std;
+
+int conjunto [5][5];
+
+for (int renglon=0;renglon<5,renglon++){
+    
+}
